522139cfd95db6aea02d957d3c566ae4c0fc7260
CrowMother <94501083+CrowMother@users.noreply.github.com>
2024-09-27 11:39:03 -0500

    upload to work on the go

--- Modules/data.py
+++ Modules/data.py
@@ -10,7 +10,17 @@ TRADES_DICT = {}
 class Trade:
     def __init__(self):
         self.schwabOrderID = ""
-        self.subTrades = []
+        self.tradeType = ""
+        self.porcessingStep = "" #Based off the furthest along step in the trade
+        self.tradeStatus = "" #Order completed (opening), working(position is open), close() , used for checking if trade is complete to calcualte win loss rate
+
+        self.openExecutionPrice = ""
+        self.closeExecutionPrice = ""
+
+        self.subTrades = [] #binds all subtrades of this 
+
+        
+
 
     # Method to check for unique tradeStatus
     def add_sub_trade(self, sub_trade):
@@ -19,6 +29,37 @@ class Trade:
             print(f"Error: Duplicate tradeStatus '{sub_trade.tradeStatus}' found in Trade '{self.schwabOrderID}'.")
         #add the duplicate trade no matter what for now, look into how often if ever this happens and if it is an issue in the future
         self.subTrades.append(sub_trade)
+
+    #function to check what kind of trade this will be
+    # Method for checking for standard or multiLeg positions
+    def check_trade_type(self):
+        # Assuming all subTrades should have the same multiLegStrategyType in a trade
+        for sub_trade in self.subTrades:
+            print(sub_trade.multiLegStrategyType)
+            if sub_trade.multiLegStrategyType:  # Check if multiLegStrategyType is not empty
+                # Identify the strategy type based on multiLegStrategyType
+                strategy_type = sub_trade.multiLegStrategyType
+                if strategy_type == "VerticalSpread":
+                    print(f"Trade {self.schwabOrderID} is a Vertical Spread.")
+                    tradeType =  "Vertical Spread"
+                elif strategy_type == "CalendarSpread":
+                    print(f"Trade {self.schwabOrderID} is a Calendar Spread.")
+                    tradeType = "Calendar Spread"
+                elif strategy_type == "IronCondor":
+                    print(f"Trade {self.schwabOrderID} is an Iron Condor.")
+                    tradeType = "Iron Condor"
+                # Add more strategies here as needed
+                elif strategy_type == "N/F":
+                    print(f"Trade {self.schwabOrderID} is a Regular Trade.")
+                    tradeType = "Regular"
+                else:
+                    print(f"Trade {self.schwabOrderID} has an unknown multi-leg strategy: {strategy_type}")
+                    tradeType = "Unknown Strategy"
+            else:
+                print(f"Trade {self.schwabOrderID} is not found.")
+                tradeType = "N/F"
+
+        self.tradeType = tradeType
                
     #tracks parts of the trade based on status of the trade
 class SubTrade():
@@ -28,19 +69,31 @@ class SubTrade():
         shortDescriptionText = ""
         executionPrice = ""
         executionSignScale = ""
+        underlyingSymbol = ""
         routedAmount = "" #figure out what the acutal value for multilegs are for this
         multiLegLimitPriceType = ""
         multiLegStrategyType = ""
         
+        
 
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #start of the data processing pipeline
 def data_in(data):
     
-    load_trade(data)
+    trade = load_trade(data)
 
-    #function to check what kind of trade this will be
-    #assuming basic trade for this test
+    #get the type of trade to check for multiLeg
+    trade.check_trade_type()
+    
+    #check what type of trade it is looking at
+
+    #regular trades, check if data is ready to send then send
+    if trade.tradeType == "Regular":
+        #prep to send data that is needed for a regular trade
+        print(trade.tradeStatus)
+
+
+
 
     #function to check if all data needed for that trade is present
 
@@ -50,39 +103,46 @@ def data_in(data):
 
 #load data into the sub trades and subtrades into the trade class and return the current trade loaded
 def load_trade(data):
-    
-    #store and load the data into objects
-    schwabOrderID = data[0]['SchwabOrderID']
+    try:
+        #store and load the data into objects
+        schwabOrderID = data[0]['SchwabOrderID']
 
 
-    # If this SchwabOrderID is not in the trades_dict, create a new Trade
-    if schwabOrderID not in TRADES_DICT:
-        trade = Trade()
-        trade.schwabOrderID = schwabOrderID
+        # If this SchwabOrderID is not in the trades_dict, create a new Trade
+        if schwabOrderID not in TRADES_DICT:
+            trade = Trade()
+            trade.schwabOrderID = schwabOrderID
+            TRADES_DICT[schwabOrderID] = trade
+        else:
+            #If the ID is in the trades_dict pull that trade
+            trade = TRADES_DICT[schwabOrderID]
+
+        #loop here for each of the subtrades
+        for json_data in data:
+
+            #either way create a subtrade to store the important data from the trade
+            sub_trade = SubTrade()
+            sub_trade.tradeStatus = pull_json_data(json_data, "BaseEvent.EventType")
+            sub_trade.shortDescriptionText = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.AssetOrderEquityOrderLeg.OrderLegs.0.Security.ShortDescriptionText")
+            sub_trade.executionPrice = pull_json_data(json_data, "BaseEvent.OrderFillCompletedEventOrderLegQuantityInfo.ExecutionInfo.ExecutionPrice.lo")
+            sub_trade.executionSignScale = pull_json_data(json_data, "BaseEvent.OrderFillCompletedEventOrderLegQuantityInfo.ExecutionInfo.ExecutionPrice.signScale")
+            sub_trade.multiLegLimitPriceType = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.EquityOrder.OptionsInfo.EquityOptionsInfo.MultiLegLimitPriceType")
+            sub_trade.multiLegStrategyType = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.EquityOrder.MultiLegStrategyType")
+            sub_trade.underlyingSymbol = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.AssetOrderEquityOrderLeg.OrderLegs.0.Security.UnderlyingSymbol")
+
+            #add the subtrade to the trade that is being tracked 
+            trade.add_sub_trade(sub_trade)
+
+            #move important data to the trade
+
+        #save the trade and subtrades to the dict
         TRADES_DICT[schwabOrderID] = trade
-    else:
-        #If the ID is in the trades_dict pull that trade
-        trade = TRADES_DICT[schwabOrderID]
-
-    #loop here for each of the subtrades
-    for json_data in data:
-
-        #either way create a subtrade to store the important data from the trade
-        sub_trade = SubTrade()
-        sub_trade.tradeStatus = pull_json_data(json_data, "BaseEvent.EventType")
-        sub_trade.shortDescriptionText = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.AssetOrderEquityOrderLeg.OrderLegs.0.Security.ShortDescriptionText")
-        sub_trade.executionPrice = pull_json_data(json_data, "BaseEvent.OrderFillCompletedEventOrderLegQuantityInfo.ExecutionInfo.ExecutionPrice.lo")
-        sub_trade.executionSignScale = pull_json_data(json_data, "BaseEvent.OrderFillCompletedEventOrderLegQuantityInfo.ExecutionInfo.ExecutionPrice.signScale")
-        sub_trade.multiLegLimitPriceType = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.EquityOrder.OptionsInfo.EquityOptionsInfo.MultiLegLimitPriceType")
-        sub_trade.multiLegStrategyType = pull_json_data(json_data, "BaseEvent.OrderCreatedEventEquityOrder.Order.EquityOrder.MultiLegStrategyType")
-
-        #add the subtrade to the trade that is being tracked 
-        trade.add_sub_trade(sub_trade)
-    
-    #save the trade and subtrades to the dict
-    TRADES_DICT[schwabOrderID] = trade
-    #return the trade
-    return trade
+        #return the trade
+        return trade
+    except Exception as e:
+        #look into making an error handler, something to either restart or partially restart the program
+        print(f"Error in load_trade: {e}")
+        return None
 
 
 
@@ -97,8 +157,8 @@ def pull_json_data(json_data, location):
         outputData = reduce(operator.getitem, keys, json_data)
         return outputData
     except KeyError as e:
-        print(f"KeyError: {e} is missing in the JSON data.")
+        #print(f"KeyError: {e} is missing in the JSON data.")
         return "N/F"
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred in pull_json_data: {str(e)}")
         return "error"
