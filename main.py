c23e00376011fe4fee3e68122a8fd8b60a98771c
CrowMother <94501083+CrowMother@users.noreply.github.com>
2024-10-17 00:18:20 -0500

    new update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import schwabdev #import the package
 
 print("welcome to the Schwab API test Suite")
 #links the bot to the client for placing and pulling information
-client = schwabdev.Client(<secret key>, <secret key>)  #create a client
+client = schwabdev.Client("app key", "secret key")  #create a client
 
 #grab the if it it is BTO CTO or other
 
